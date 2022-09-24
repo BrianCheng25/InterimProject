cf963286039e3d973c8d54715c3d725c8a467bdd
supernovaBvS <112676063+supernovaBvS@users.noreply.github.com>
2022-09-24 14:49:22 +0800

    updated tweets dataframe

--- env/twi-apis.py
+++ env/twi-apis.py
@@ -4,7 +4,7 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 # query format and limit
-query = " #Coronavirus until:2019-03-31 since:2019-01-01" # search by keywords
+query = " #Coronavirus until:2020-03-31 since:2020-01-01" # search by keywords
 #query = "(from:eafifaesports) until:2019-09-01 since:2018-09-01" # search by user
 tweets = [] # tweets (var) = list 
 limit = 1000 # tweets limit i wanna get
@@ -17,11 +17,13 @@ for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     if len(tweets) == limit: # Get tweet till it reached the limit set above
         break # Condition = True jau end loop
     else: # Get all tweet and put into a list 
-        tweets.append([tweet.date, tweet.username, tweet.content])
+        tweets.append([tweet.date, tweet.url, tweet.user.username, tweet.sourceLabel, tweet.user.location, tweet.content, tweet.likeCount, tweet.retweetCount,  tweet.quoteCount, tweet.replyCount])
+        #tweets.append([tweet.date, tweet.username, tweet.content])
         
 # Make the list into a dataframe by using pandas
-df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+df = pd.DataFrame(tweets, columns=['Date', 'TweetURL','User', 'Source', 'Location', 'Tweet', 'Likes_Count','Retweet_Count', 'Quote_Count', 'Reply_Count'])
+#df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
 print(df)
 
 # to save to csv
-df.to_csv('20190103tweets.csv')
+#df.to_csv('tweets.csv')
