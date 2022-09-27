8815fed8a039915cbdfea0f317b8ebd5a270e1aa
supernovaBvS <112676063+supernovaBvS@users.noreply.github.com>
2022-09-28 00:40:17 +0800

    push

--- env/twi-apis.py
+++ env/twi-apis.py
@@ -4,8 +4,8 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 # query format and limit
-#query = " #Coronavirus until:2020-03-31 since:2020-01-01" # search by keywords
-query = "(from:joebiden) until:2022-08-31 since:2021-01-01" # search by user
+query = " #Coronavirus until:2021-01-31 since:2020-01-01" # search by keywords
+#query = "(from:joebiden) until:2022-08-31 since:2021-01-01" # search by user
 tweets = [] # tweets (var) = list 
 limit = 10000 # tweets limit i wanna get
 
